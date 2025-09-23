47bb2c2cc1f901aa80faa3a71aedf1817c680ce4
YunSung Choi <sunsu6765@gmail.com>
2025-09-23 15:11:35 +0900

    fix: generateMetadata함수 props타입 수정

--- src/app/groups/[id]/page.tsx
+++ src/app/groups/[id]/page.tsx
@@ -2,6 +2,10 @@ import ReadingGroupDetails from '@/components/groups/details';
 
 import type { Metadata } from 'next';
 
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+
 export async function generateStaticParams() {
   // TODO: 백엔드에서 동적 라우팅될 목록의 id 범위 불러오기
   const tempIdArray = Array.from({ length: 10 }, (_, index) => ({
@@ -10,19 +14,17 @@ export async function generateStaticParams() {
   return tempIdArray;
 }
 
-export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const { id } = await params;
+
   // TODO: SEO를 위한 데이터 수정 필요
   return {
-    title: '{group.groupName} | 그루 독서모임',
+    title: `${id} | 그루 독서모임`,
     description: '{group.content.substring(0, 150)}'
   };
 }
 
-interface ReadingGroupDetailsPageProps {
-  params: Promise<{ id: string }>;
-}
-
-async function ReadingGroupDetailsPage({ params }: ReadingGroupDetailsPageProps) {
+async function ReadingGroupDetailsPage({ params }: PageProps) {
   const { id } = await params;
 
   return (
