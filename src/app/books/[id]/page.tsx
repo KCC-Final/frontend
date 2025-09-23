255c6af96389d1fdb189491db8337d8e45a54492
YunSung Choi <sunsu6765@gmail.com>
2025-09-23 10:53:40 +0900

    feat: 동적라우팅 페이지에 대한 정적 페이지화 설정 추가

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -1,5 +1,23 @@
 import BookDetails from '@/components/books/details';
 
+import type { Metadata } from 'next';
+
+export async function generateStaticParams() {
+  // TODO: 백엔드에서 동적 라우팅될 목록의 id 범위 불러오기
+  const tempIdArray = Array.from({ length: 10 }, (_, index) => ({
+    id: String(index + 1)
+  }));
+  return tempIdArray;
+}
+
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  // TODO: SEO를 위한 데이터 수정 필요
+  return {
+    title: '{book.title} | 그루 도서정보',
+    description: '{book.desciption.substring(0, 150)}'
+  };
+}
+
 function BookDetailsPage() {
   return <BookDetails />;
 }
--- src/app/reviews/[id]/page.tsx
+++ src/app/reviews/[id]/page.tsx
@@ -1,5 +1,23 @@
 import ReviewDetails from '@/components/reviews/details';
 
+import type { Metadata } from 'next';
+
+export async function generateStaticParams() {
+  // TODO: 백엔드에서 동적 라우팅될 목록의 id 범위 불러오기
+  const tempIdArray = Array.from({ length: 10 }, (_, index) => ({
+    id: String(index + 1)
+  }));
+  return tempIdArray;
+}
+
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  // TODO: SEO를 위한 데이터 수정 필요
+  return {
+    title: '{review.reviewTitle} | 그루 독후감',
+    description: '{review.reviewContent.substring(0, 150)}'
+  };
+}
+
 function ReviewDetailsPage() {
   return <ReviewDetails />;
 }
