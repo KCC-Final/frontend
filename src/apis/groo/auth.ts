1e04a45dccca987d1586a5884a8cdca8c348f9ee
YunSung Choi <sunsu6765@gmail.com>
2025-10-10 16:26:44 +0900

    feat: 미들웨어를 통한 페이지 접속시 토큰 확인 후 리다이렉트 설정 추가

--- src/apis/groo/auth.ts
+++ src/apis/groo/auth.ts
@@ -1,16 +1,32 @@
 import axiosGroo from '@/apis/groo/config';
-import { CommonResDTO, LoginReqBody } from '@/types';
+import { LoginReqBody, LoginResDTO, LogoutResDTO, ReissueTokenResDTO } from '@/types';
 
 export const auth = {
   // 로그인
-  login: async (data: LoginReqBody): Promise<CommonResDTO> => {
+  login: async (data: LoginReqBody): Promise<LoginResDTO> => {
     const response = await axiosGroo.post('/auth/login', data);
     return response.data;
   },
 
   // 로그아웃
-  logout: async () => {
+  logout: async (): Promise<LogoutResDTO> => {
     const response = await axiosGroo.post('/auth/logout');
     return response.data;
+  },
+
+  // 엑세스토큰 재발행
+  reissueToken: async (): Promise<ReissueTokenResDTO> => {
+    const response = await axiosGroo.post('/token-refresh');
+    return response.data;
+  },
+
+  // 프론트서버에서 엑세스토큰 재발행
+  reissueTokenInServer: async (refreshToken: string) => {
+    const response = await axiosGroo.post(
+      '/token-refresh',
+      {},
+      { headers: { Cookie: `refreshToken=${refreshToken}` } }
+    );
+    return response;
   }
 };
--- src/types/auth/dto.ts
+++ src/types/auth/dto.ts
@@ -2,7 +2,10 @@ import { CommonResDTO } from '@/types';
 
 // 로그인
 export type LoginReqBody = { userId: string; password: string };
-export type LoginResDTO = CommonResDTO<null>;
+export type LoginResDTO = CommonResDTO<{ accessToken: string; refreshToken: string }>;
 
 // 로그아웃
 export type LogoutResDTO = CommonResDTO<null>;
+
+// 토큰 재발행
+export type ReissueTokenResDTO = CommonResDTO<string>;
