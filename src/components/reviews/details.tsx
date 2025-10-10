deaf99bb37ebc77dfaf3c27bc1886049b99974d8
umyunho <kolgu96@naver.com>
2025-10-10 11:25:21 +0900

    fix: tiptop 설정 오류 문제  해결
    - tiptop 설정을 위해 따로 scss파일 생성
    - review edit/update 페이지에서 사용
    - 글자수 실시간 반영 추가
    - 형광,표,밑줄  기능 추가
    - 사용하지 않는 페이지 삭제

--- src/components/reviews/details.tsx
+++
@@ -1,5 +0,0 @@
-function ReviewDetails() {
-  return <div>독후감 상세페이지입니다.</div>;
-}
-
-export default ReviewDetails;
--- src/components/reviews/write.tsx
+++
@@ -1,5 +0,0 @@
-function ReviewWrite() {
-  return <div>독후감 작성 페이지입니다.</div>;
-}
-
-export default ReviewWrite;
