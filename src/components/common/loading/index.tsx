cd255cbcfe71e8b7244e29f5a162d13f1982f62a
YunSung Choi <sunsu6765@gmail.com>
2025-10-27 12:00:12 +0900

    feat: 페이지 로딩 상태 UI 컴포넌트 생성

---
+++ src/components/common/loading/index.tsx
@@ -0,0 +1,18 @@
+import clsx from 'clsx';
+
+import styles from '@/components/common/loading/loading.module.scss';
+
+interface PageLoadingProps {
+  isLoading?: boolean;
+}
+
+function PageLoading({ isLoading }: PageLoadingProps) {
+  return (
+    <section className={clsx(styles.loading, { [styles.hidden]: !isLoading })}>
+      <div className={styles.spinner} />
+      <p>로딩 중...</p>
+    </section>
+  );
+}
+
+export default PageLoading;
