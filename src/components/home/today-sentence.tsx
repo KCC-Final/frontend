cb299bf574c0afdc1403e08a1ae0d59f3deddb9a
umyunho <kolgu96@naver.com>
2025-11-09 16:53:41 +0900

    design: 오늘의 한문장 백그라운드 이미지 지정 및 스타일 수정

--- src/components/home/today-sentence.tsx
+++ src/components/home/today-sentence.tsx
@@ -3,6 +3,7 @@
 import { format } from 'date-fns';
 import Link from 'next/link';
 
+import backgroundImg from '@/assets/sentence_background.png';
 import styles from '@/components/home/home.module.scss';
 import { useHomeStore } from '@/stores/home';
 import { formatBookAuthor, formatBookTitle } from '@/utils/format/string';
@@ -16,15 +17,19 @@ function TodaySentence() {
 
   return (
     <section className={styles.quote}>
-      <Link href={`/books/${quoteData.sentence.isbn}`}>
+      <Link
+        href={`/books/${quoteData.sentence.isbn}`}
+        style={{
+          backgroundImage: `url(${backgroundImg.src})`
+        }}>
         <div className={styles.content}>
-          <div className={styles.date}>{format(new Date(quoteData.sentence.selectedDate), 'yyyy-MM-dd')}</div>
           <div className={styles.sentence}>
             <p>{quoteData.sentence.sentenceContent}</p>
           </div>
           <div className={styles.description}>
-            <div>{formatBookTitle(quoteData.book.title)}</div>
-            <div>{formatBookAuthor(quoteData.book.author)}</div>
+            {/* 도서 제목에 겹낫표 추가 */}
+            <div className={styles.bookTitle}>「{formatBookTitle(quoteData.book.title)}」</div>
+            <div className={styles.bookAuthor}>{formatBookAuthor(quoteData.book.author)}</div>
           </div>
         </div>
       </Link>
