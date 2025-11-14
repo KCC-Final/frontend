a7c272c69d96841608abf893c221b26c9ae58adc
umyunho <kolgu96@naver.com>
2025-11-14 12:58:17 +0900

    feat:도서 검색시 검색란에서 도서 상세 이동 구현 추가

--- src/types/search/dto.ts
+++ src/types/search/dto.ts
@@ -1,11 +1,11 @@
 import { CommonResDTO } from '@/types';
 
 export type SearchResultItem = {
-  category: 'USER' | 'REVIEW' | 'COMMENT';
-  id: string;
-  title: string;
-  subtext: string;
-  content: string;
+  category: 'USER' | 'REVIEW' | 'COMMENT' | 'BOOK'; // BOOK 추가
+  id: string; // book은 isbn13
+  title: string; // book.title
+  subtext: string; // book.author
+  content?: string; // BOOK에는 없으므로 optional 처리
 };
 
 export type SearchResDTO = CommonResDTO<SearchResultItem[]>;
